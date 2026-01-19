9960daa47adc8e810746a24b8d9357061dedb9b9
camiloandreeacevedo <camilo.andree.ac@gmail.com>
2026-01-19 20:28:08 -0300

    Create OrderItem.js

---
+++ WebSide/src/models/OrderItem.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const OrderItem = sequelize.define('OrderItem', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    order_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    price_at_moment: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    }
+}, {
+    tableName: 'order_items',
+    timestamps: false
+});
+
+module.exports = OrderItem;
