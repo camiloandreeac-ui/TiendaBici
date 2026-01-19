10530647c45acef7880cd9cc758ecf287002a135
camiloandreeacevedo <camilo.andree.ac@gmail.com>
2026-01-19 20:28:12 -0300

    Create Product.js

---
+++ WebSide/src/models/Product.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Product = sequelize.define('Product', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING
+    },
+    image_url: {
+        type: DataTypes.STRING
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+}, {
+    tableName: 'products',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: false
+});
+
+module.exports = Product;
